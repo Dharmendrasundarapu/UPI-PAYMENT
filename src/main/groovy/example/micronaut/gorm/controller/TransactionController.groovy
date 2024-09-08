4a4b68386f6d09fb5cf75e441c1bff0ed167f550
Dharmendrasundarapu <155229799+Dharmendrasundarapu@users.noreply.github.com>
2024-09-08 18:01:29 +0530

    updated with kafka

--- src/main/groovy/example/micronaut/gorm/controller/TransactionController.groovy
+++ src/main/groovy/example/micronaut/gorm/controller/TransactionController.groovy
@@ -7,20 +7,37 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Post
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 import javax.inject.Inject
 
+
 @Controller("/transaction")
 class TransactionController {
+    private static final Logger LOG= LoggerFactory.getLogger(TransactionController.class)
 
     @Inject
-    TransactionService transactionService
+    TransactionService transactionService;
+
     @Post("/transfer")
-    def moneyTransfer(@Body TransactionModel transactionModel)
-    {
-        return transactionService.transferMoney(transactionModel.senderMobileNumber,
-        transactionModel.receiverMobileNumber,transactionModel.amount,transactionModel.upiPin)
+    public String transferMoney(@Body TransactionModel transactionModel) {
+        LOG.info("Received transaction: {}", transactionModel);
+
+        boolean result = transactionService.transferMoney(
+                transactionModel.getSenderMobileNumber(),
+                transactionModel.getReceiverMobileNumber(),
+                transactionModel.getAmount(),
+                transactionModel.getUpiPin()
+        );
+
+        if (result) {
+            return "Transfer successful";
+        } else {
+            return "Transfer failed";
+        }
     }
+
     @Get("/history/{phoneNumber}")
     def transactionHistory(@PathVariable Long phoneNumber) {
         return transactionService.getTransactionHistory(phoneNumber)
